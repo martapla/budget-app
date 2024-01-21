145da7dd451595e0561d54afeacb54cee3b8d5a3
martapla <martaplanella@hotmail.com>
2024-01-21 16:00:33 +0100

    close modal

--- src/components/CardWeb.jsx
+++ src/components/CardWeb.jsx
@@ -55,8 +55,13 @@ const CardWeb = ({ onShowClick, updatePrices }) => {
   //   }
   
 function displayModal() {
-  setShowModal(!showModal)
-}
+  setShowModal(!showModal);
+  }
+  
+  function closeModal() {
+  if (showModal)
+  setShowModal(false);
+  }
 
 return (
   
@@ -146,13 +151,13 @@ return (
       )}
 
       {showModal && (
-        <div className='bg-gray-400 bg-opacity-75 z-50 absolute top-0 h-full w-full flex items-center justify-center'>
-          <div className='m-4 p-6 bg-gray-100 border-2 rounded-lg border-blue-300'>
-            <h2 className='py-4 text-blue-600 font-bold'> Number of pages</h2>
-            <p className='text-blue-600'> Add quantity of pages you need.</p>
-            <p className='text-blue-600'>  Each page has a cost of <span className='text-blue-600 font-bold'>30€</span> </p>
-         
-          </div>
+        <div className='bg-gray-400 bg-opacity-75 z-50 absolute top-0 h-full w-full flex items-center justify-center' onClick={closeModal}>
+            <div className='m-4 p-6 bg-gray-100 border-2 rounded-lg border-blue-300' onClick={closeModal}>
+              <h2 className='py-4 text-blue-600 font-bold'> Number of pages</h2>
+              <p className='text-blue-600'> Add quantity of pages you need.</p>
+              <p className='text-blue-600'>  Each page has a cost of <span className='text-blue-600 font-bold'>30€</span> </p>
+          
+            </div>
         </div>
        
        )}
